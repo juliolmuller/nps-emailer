e8f7f63fa835cba09aba435a26bbbde7a2654ad7
Julio Muller <juliodolago@gmail.com>
2021-03-26 02:30:28 -0300

    Created model/entity and first route to save new user

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from 'express'
+import { User } from '../models'
 
 class UserController {
-  async create(request: Request, response: Response) {
-    console.log(request.body)
 
-    response.send()
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body
+    const existingUser = await User.findOne({ email })
+    console.log(existingUser)
+    if (existingUser) {
+      return response.status(400).json({
+        error: `User with email "${email} already exists.`,
+      })
+    }
+
+    const user = new User()
+
+    user.name = name
+    user.email = email
+    await user.save()
+
+    return response.status(201).json(user)
   }
 }
 
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,4 +1,19 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
-class User {}
+@Entity()
+class User extends BaseEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @CreateDateColumn()
+  createdAt: Date
+}
 
 export default User
