a082b2443e64383a1639d08fc59c12c27d4c6688
Julio Muller <juliodolago@gmail.com>
2021-03-26 01:57:19 -0300

    ConfiguredORM name strategy and removed explicit use of dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,38 @@
+import { createConnection, DefaultNamingStrategy, getConnectionOptions } from 'typeorm'
+import snakeCase from 'lodash.snakecase'
+import pluralize from 'pluralize'
 import 'reflect-metadata'
-import { createConnection } from 'typeorm'
-import dotenv from 'dotenv'
 
-createConnection()
-dotenv.config()
+/**
+ * As TypeORM depends on the values stored on environment
+ * variables, the function below already runs a "dotenv"-like
+ * job and make ".env" values available  globally in Node
+ * "process.env". Therefore, there's no need to run "dotenv"
+ * package explicitly.
+ */
+getConnectionOptions().then((connectionOptions) => {
+  const namingStrategy = new DefaultNamingStrategy()
+
+  // Default table name becomes the plural of model name
+  namingStrategy.tableName = (targetName, userSpecifiedName) => {
+    if (userSpecifiedName) {
+      return userSpecifiedName
+    }
+
+    const nameParts = snakeCase(targetName).split('_')
+    const [lastName] = nameParts.splice(-1)
+    const pluralName = pluralize(lastName)
+
+    return [...nameParts, pluralName].join('_')
+  }
+
+  // Transform pascal-cased properties to snake-cased columns names
+  namingStrategy.columnName = (propertyName, customName, embeddedPrefixes) => {
+    const prefix = snakeCase(embeddedPrefixes.concat('').join('_'))
+    const columnName = customName ?? snakeCase(propertyName)
+
+    return prefix + columnName
+  }
+
+  return createConnection({ ...connectionOptions, namingStrategy })
+})
