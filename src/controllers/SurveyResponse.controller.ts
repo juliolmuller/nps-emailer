4800e87ccb4f496db15b3d5bdb1fbac9bf54f250
Julio Muller <juliodolago@gmail.com>
2021-03-28 01:05:05 -0300

    Implemented update to save user's grade

--- src/controllers/SurveyResponse.controller.ts
+++ src/controllers/SurveyResponse.controller.ts
@@ -6,11 +6,22 @@ class SurveyResponseController {
 
   // Edit existing survey response, adding user grade
   async respondTo(request: Request, response: Response) {
-    // TODO: to implement
-    response.json({
-      params: request.params,
-      query: request.query,
-    })
+    const { id } = request.params
+    const { grade } = request.query
+    const surveyResponse = await SurveyResponse.findOne({ id: String(id) })
+
+    if (!surveyResponse) {
+      return response
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ error: 'Survey response could not be processed.' })
+    }
+
+    surveyResponse.value = Number(grade)
+    await surveyResponse.save()
+
+    return response
+      .status(StatusCodes.OK)
+      .json({ message: 'Survey response successfully processed.' })
   }
 }
 
